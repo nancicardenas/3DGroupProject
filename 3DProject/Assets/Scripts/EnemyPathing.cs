378f959f06aa8336befb5076ab34937ea061944a
AidenNSu <161399280+AidenNSu@users.noreply.github.com>
2025-11-20 19:45:06 -0800

    Enemy pathing

---
+++ 3DProject/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPathing : MonoBehaviour
+{
+
+    public NavMeshAgent navMesh;
+
+    //public transform player;
+
+    //public layerMask isGround, isplayer;
+    public Vector3 walkPoint;
+    bool destination;
+    
+    public float sightRange;
+    public bool playerInRange;
+
+
+
+    private void Patorl(){
+
+    }
+
+    private void ChasePlayer(){
+
+    }
+
+}
